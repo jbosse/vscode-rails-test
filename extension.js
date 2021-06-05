5082f2dfbccf7640d919905c0b10d41a9c9a4f7c
Jimmy Bosse <jimmy@funkyboss.com>
2021-06-05 17:05:01 -0400

    Refactor and test command.

--- extension.js
+++ extension.js
@@ -1,5 +1,7 @@
 const vscode = require("vscode");
+const { command } = require("./command");
 
+//#region
 function findTests(document, kind) {
   const testRegex = new RegExp(`\\s*${kind}\\s+.(.*).\\s+do`);
   const tests = [];
@@ -16,46 +18,7 @@ function findTests(document, kind) {
   }
   return tests;
 }
-
-function findIts(document) {
-  const its = [];
-  for (let i = 0; i < document.lineCount; i++) {
-    const line = document.lineAt(i);
-    const index = line.text.indexOf("it");
-    if (index >= 0) {
-      its.push({
-        line: i,
-        range: new vscode.Range(i, index, i, line.text.length)
-      });
-    }
-  }
-  return its;
-
-}
-
-function runFile(terminal, _index, document) {
-  console.log(document);
-  const path = document.fileName.replace(`${vscode.workspace.rootPath}/`, "");
-  const command = `bin/rails test ${path}`;
-  terminal.sendText(command);
-}
-
-function runDescribe(terminal, index, document) {
-  const line = document.lineAt(index);
-  const describeRegex = /\s*describe\s+.(.*).\s+do/;
-  const path = document.fileName.replace(`${vscode.workspace.rootPath}/`, "");
-  const testName = line.text.match(describeRegex)[1];
-  const testRegex = testName.replace(/[^a-zA-Z]/g, ".");
-  const command = `bin/rails test ${path} --name /${testRegex}/`;
-  terminal.sendText(command);
-}
-
-function runTest(terminal, index, document) {
-  const path = document.fileName.replace(`${vscode.workspace.rootPath}/`, "");
-  const command = `bin/rails test ${path}:${index + 1}`;
-  terminal.sendText(command);
-}
-
+//#endregion
 function activate(context) {
 
   // Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -65,30 +28,16 @@ function activate(context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("rails.lens.test.run", function (type, index, document) {
-    // `bin/rails -t test/controllers/my_controller_test.rb -n /my\ test\ description/`
-    const railsTestsTerminal =
-      vscode.window.terminals.find((terminal) => terminal.name == "Rails Tests") ||
-      vscode.window.createTerminal("Rails Tests");
+  let disposable = vscode.commands.registerCommand("rails.lens.test.run", command);
 
-    railsTestsTerminal.show();
-    railsTestsTerminal.sendText("clear");
-    if (type === "file") {
-      runFile(railsTestsTerminal, index, document);
-    }
-    if (type === "describe") {
-      runDescribe(railsTestsTerminal, index, document);
-    }
-    if (type === "it" || type === "test") {
-      runTest(railsTestsTerminal, index, document);
-    }
-  });
+  context.subscriptions.push(disposable);
 
   let docSelector = {
     language: "ruby",
     scheme: "file"
   };
 
+  //#region
   // Register our CodeLens provider
   let fileLensProvider = vscode.languages.registerCodeLensProvider(
     docSelector,
@@ -142,11 +91,12 @@ function activate(context) {
     }
   );
 
-  context.subscriptions.push(disposable);
+
   context.subscriptions.push(fileLensProvider);
   context.subscriptions.push(describeLensProvider);
   context.subscriptions.push(testLensProvider);
   context.subscriptions.push(itLensProvider);
+  //#endregion
 }
 
 // this method is called when your extension is deactivated
--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -1,15 +1,170 @@
 const assert = require('assert');
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 const vscode = require('vscode');
-// const myExtension = require('../extension');
+const { runFile, runDescribe, runTest, command } = require("../../command");
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  test("runFile will send the command to the terminal", () => {
+    let actualCommand = "";
+    const fakeTerminal = { sendText: function (cmd) { actualCommand = cmd; } };
+    const fakeDocument = { fileName: `${vscode.workspace.rootPath}/my_file_name.something` };
+
+    runFile(fakeTerminal, fakeDocument);
+
+    assert.strictEqual("bin/rails test my_file_name.something", actualCommand);
+  });
+
+  test("runDescribe will send the command to the terminal", () => {
+    const index = Math.floor((Math.random() * 10) + 1);
+    let actualCommand = "";
+    const fakeTerminal = { sendText: function (cmd) { actualCommand = cmd; } };
+    const fakeDocument = {
+      lineAt: function (idx) {
+        if (idx == index) {
+          return { text: "     describe \"some rando'm test 122\" do         " };
+        }
+        return "";
+      },
+      fileName: `${vscode.workspace.rootPath}/my_file_name.something`
+    };
+
+    runDescribe(fakeTerminal, index, fakeDocument);
+
+    assert.strictEqual("bin/rails test my_file_name.something --name /some.rando.m.test..../", actualCommand);
+  });
+
+  test("runTest will send the command to the terminal", () => {
+    const index = Math.floor((Math.random() * 10) + 1);
+    let actualCommand = "";
+    const fakeTerminal = { sendText: function (cmd) { actualCommand = cmd; } };
+    const fakeDocument = { fileName: `${vscode.workspace.rootPath}/my_file_name.something` };
+
+    runTest(fakeTerminal, index, fakeDocument);
+
+    assert.strictEqual(`bin/rails test my_file_name.something:${index + 1}`, actualCommand);
+  });
+
+  test("command will create a terminal when none exists", () => {
+    let terminalName = "";
+    let sendTexts = [];
+    let showCalled = false;
+    const index = Math.floor((Math.random() * 10) + 1);
+    const fakeTerminal = {
+      show: function () { showCalled = true; },
+      sendText: function (cmd) { sendTexts.push(cmd); }
+    };
+    const fakeDocument = { fileName: `${vscode.workspace.rootPath}/my_file_name.something` };
+    vscode.window = {
+      terminals: [],
+      createTerminal: function (name) {
+        terminalName = name;
+        return fakeTerminal;
+      }
+    };
+
+    command("", index, fakeDocument);
+
+    assert.strictEqual(`Rails Tests`, terminalName);
+    assert.strictEqual(true, showCalled);
+    assert.strictEqual("clear", sendTexts[0]);
+  });
+
+  test("command will use existing terminal", () => {
+    let terminalName = "";
+    let sendTexts = [];
+    let showCalled = false;
+    const index = Math.floor((Math.random() * 10) + 1);
+    const fakeTerminal = {
+      name: "Rails Tests",
+      show: function () { showCalled = true; },
+      sendText: function (cmd) { sendTexts.push(cmd); }
+    };
+    const fakeDocument = { fileName: `${vscode.workspace.rootPath}/my_file_name.something` };
+    vscode.window = {
+      terminals: [fakeTerminal],
+      createTerminal: function (name) {
+        terminalName = name;
+        return null;
+      }
+    };
+
+    command("", index, fakeDocument);
+
+    assert.strictEqual("", terminalName);
+    assert.strictEqual(true, showCalled);
+    assert.strictEqual("clear", sendTexts[0]);
+  });
+
+  test("will run for the file", () => {
+    let sendTexts = [];
+    const index = Math.floor((Math.random() * 10) + 1);
+    const fakeTerminal = {
+      name: "Rails Tests",
+      show: function () { /*noop*/ },
+      sendText: function (cmd) { sendTexts.push(cmd); }
+    };
+    const fakeDocument = { fileName: `${vscode.workspace.rootPath}/my_file_name.something` };
+    vscode.window = { terminals: [fakeTerminal] };
+
+    command("file", index, fakeDocument);
+
+    assert.strictEqual("bin/rails test my_file_name.something", sendTexts[1]);
+  });
+
+  test("will run for the describe", () => {
+    let sendTexts = [];
+    const index = Math.floor((Math.random() * 10) + 1);
+    const fakeTerminal = {
+      name: "Rails Tests",
+      show: function () { /*noop*/ },
+      sendText: function (cmd) { sendTexts.push(cmd); }
+    };
+    const fakeDocument = {
+      lineAt: function (idx) {
+        if (idx == index) {
+          return { text: "     describe \"some rando'm test 122\" do         " };
+        }
+        return "";
+      },
+      fileName: `${vscode.workspace.rootPath}/my_file_name.something`
+    }
+    vscode.window = { terminals: [fakeTerminal] };
+
+    command("describe", index, fakeDocument);
+
+    assert.strictEqual("bin/rails test my_file_name.something --name /some.rando.m.test..../", sendTexts[1]);
+  });
+
+  test("will run for the test", () => {
+    let sendTexts = [];
+    const index = Math.floor((Math.random() * 10) + 1);
+    const fakeTerminal = {
+      name: "Rails Tests",
+      show: function () { /*noop*/ },
+      sendText: function (cmd) { sendTexts.push(cmd); }
+    };
+    const fakeDocument = { fileName: `${vscode.workspace.rootPath}/my_file_name.something` };
+    vscode.window = { terminals: [fakeTerminal] };
+
+    command("test", index, fakeDocument);
+
+    assert.strictEqual(`bin/rails test my_file_name.something:${index + 1}`, sendTexts[1]);
+  });
+
+  test("will run for the it", () => {
+    let sendTexts = [];
+    const index = Math.floor((Math.random() * 10) + 1);
+    const fakeTerminal = {
+      name: "Rails Tests",
+      show: function () { /*noop*/ },
+      sendText: function (cmd) { sendTexts.push(cmd); }
+    };
+    const fakeDocument = { fileName: `${vscode.workspace.rootPath}/my_file_name.something` };
+    vscode.window = { terminals: [fakeTerminal] };
+
+    command("it", index, fakeDocument);
+
+    assert.strictEqual(`bin/rails test my_file_name.something:${index + 1}`, sendTexts[1]);
+  });
 });
